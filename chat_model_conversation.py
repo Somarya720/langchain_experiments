3f39f98da072876f5ad30cb9791996129d77ca4b
Somarya720 <somaryabhattacharyya@gmail.com>
2025-07-20 21:59:05 +0530

    add AI message to the conversation history

--- chat_model_conversation.py
+++ chat_model_conversation.py
@@ -4,15 +4,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-messages = [
-    SystemMessage("You are an expert in social media content strategy"),
-    HumanMessage("Give a short tip to create engaging postson instagram")
-]
+# messages = [
+#     SystemMessage("You are an expert in social media content strategy"),
+#     HumanMessage("Give a short tip to create engaging postson instagram")
+# ]
 
 llm = ChatOpenAI(model='gpt-3.5-turbo')
 
-result = llm.invoke(messages)
+messages = [
+    SystemMessage("You are a bollywood movie suggestion expert"),
+    HumanMessage("Suggest one good hindi movie"),
+]
 
-print(result.content)
+suggestion_response = llm.invoke(messages)
+suggestion = suggestion_response.content
 
+print(f'AI Suggestion: {suggestion}')
+
+# Adding AI response
+messages.extend([
+    AIMessage(suggestion),
+    HumanMessage("Name the actors of the movie")
+])
+
+actor_response = llm.invoke(messages)
+print(f"Actors: {actor_response.content}")
 
