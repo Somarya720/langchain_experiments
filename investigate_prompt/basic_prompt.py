c16aeb2459a791fc913cb0cd4242535f76118763
Somarya720 <somaryabhattacharyya@gmail.com>
2025-07-21 00:12:50 +0530

    add function to generate prompts from template using langchain prompt library

--- investigate_prompt/basic_prompt.py
+++ investigate_prompt/basic_prompt.py
@@ -1,16 +1,26 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
-def invoke_llm_without_prompt(template: str, data: dict, model: ChatOpenAI) -> str:
+def invoke_llm_from_template(template: str, data: dict, model: ChatOpenAI) -> str:
     '''
-    generates ai response from llm without using langchain prompt templates
+    generates ai response from llm without using langchain prompt templates support
     '''
     prompt = template.format(**data) # using inbuilt string format to add values to the template
     response = model.invoke(prompt)
     return response.content
 
+
+def invoke_llm_from_template_langchain_prompt(template: str, data: dict, model: ChatOpenAI) -> str:
+    '''
+    generates ai response from llm using langchain prompt templates support
+    '''
+    prompt_template = ChatPromptTemplate.from_template(template) # converts to template object
+    prompt = prompt_template.invoke(data) # generates prompt value
+    response = model.invoke(prompt)
+    return response.content
 
 llm = ChatOpenAI(model='gpt-3.5-turbo')
 
@@ -31,5 +41,5 @@ data_set = [
 ]
 
 for data in data_set:
-    response = invoke_llm_without_prompt(template, data, llm)
+    response = invoke_llm_from_template_langchain_prompt(template, data, llm)
     print(response, end="\n-----------------------------\n")
