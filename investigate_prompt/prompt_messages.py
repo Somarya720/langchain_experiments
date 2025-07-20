380775aa5653d967dc160747308b739cb177b92f
Somarya720 <somaryabhattacharyya@gmail.com>
2025-07-21 00:36:12 +0530

    add code to generate prompt from template list

---
+++ investigate_prompt/prompt_messages.py
@@ -0,0 +1,21 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(model='gpt-3.5-turbo')
+
+messages_template_list = [
+    ('system', 'You are a subject matter expert in {subject}'),
+    ('human', 'How to create a {object} in {subject}')
+]
+
+template = ChatPromptTemplate.from_messages(messages_template_list)
+prompt = template.invoke({
+    'subject': 'c#',
+    'object': 'function'
+})
+
+response = llm.invoke(prompt)
+print(response.content)
