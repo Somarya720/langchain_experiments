eb4639c9294103e1457f34ab07993c580fb27a62
Somarya720 <somaryabhattacharyya@gmail.com>
2025-07-20 22:32:28 +0530

    add chat history

--- basic_chat_app.py
+++ basic_chat_app.py
@@ -19,10 +19,17 @@ while(True):
         print('You have ended the chat')
         break
 
+    # Add user message
     chat_history.append(HumanMessage(content=user_msg))
 
+    # Fetch ai response based on history of conversation
     ai_response = llm.invoke(chat_history)
     ai_msg = ai_response.content
+
+    # Add ai message to chat history
     chat_history.append(AIMessage(ai_msg))
 
     print(f"AI: {ai_msg}")
+
+print("------Chat History------")
+print(chat_history)
