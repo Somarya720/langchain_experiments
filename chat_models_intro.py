59027dd8dd465ccc6157842e99080fee5f986f74
Somarya720 <somaryabhattacharyya@gmail.com>
2025-07-20 20:32:09 +0530

    load secret key to the program

--- chat_models_intro.py
+++ chat_models_intro.py
@@ -1,8 +1,11 @@
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
 
+# Loads the secret key to the environment variable from .env
+load_dotenv()
 
-# llm = ChatOpenAI(model="gpt-3.5-turbo")
+llm = ChatOpenAI(model="gpt-3.5-turbo")
 
-# result = llm.invoke("What is the square root of 49")
+result = llm.invoke("What is the square root of 49")
 
-# print(result)
+print(result)
