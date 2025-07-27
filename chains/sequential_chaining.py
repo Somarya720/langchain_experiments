276fbec14391c5f0804f8470e4ef8e5c1ce966d0
Somarya720 <somaryabhattacharyya@gmail.com>
2025-07-27 16:14:36 +0530

    add more tasks in llm

---
+++ chains/sequential_chaining.py
@@ -0,0 +1,37 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Tasks: format the template, invoke the llm, extract the output, use it in translation template, invoke llm, extract the output
+
+model = ChatOpenAI(model='gpt-4.1-nano')
+
+fact_template = [
+    ("system", "You are a sports expert in the field of {game}"),
+    ("human", "Who won the {tournament} in {year}?")
+]
+fact_data = {
+    'game': 'football',
+    'tournament': 'World cup',
+    'year': 2022
+}
+
+fact_prompt_template = ChatPromptTemplate.from_messages(fact_template)
+fact_prompt = fact_prompt_template.invoke(fact_data)
+fact_response = model.invoke(fact_prompt)
+
+translate_template = [
+    ("system", "You are a translation expert"),
+    ("human", "Translate the text {text} to {language}")
+]
+translation_data = {
+    'text': fact_response.content,
+    'language': 'Portugal'
+}
+translate_prompt_template = ChatPromptTemplate.from_messages(translate_template)
+translate_prompt = translate_prompt_template.invoke(translation_data)
+translation_response = model.invoke(translate_prompt)
+
+print(fact_response.content, translation_response.content, sep='\n\n')
