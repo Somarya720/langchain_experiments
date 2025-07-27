2ab8ac9c5e5e209bbd7108dde4d11ed3e2a5468b
Somarya720 <somaryabhattacharyya@gmail.com>
2025-07-27 16:33:16 +0530

    convert the sequence of tasks using chains

--- chains/sequential_chaining.py
+++ chains/sequential_chaining.py
@@ -1,5 +1,7 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,30 +10,51 @@ load_dotenv()
 
 model = ChatOpenAI(model='gpt-4.1-nano')
 
+# Prepare the templates
 fact_template = [
     ("system", "You are a sports expert in the field of {game}"),
     ("human", "Who won the {tournament} in {year}?")
 ]
+translate_template = [
+    ("system", "You are a translation expert"),
+    ("human", "Translate the text {text} to {language}")
+]
+
+prompt_fact_template = ChatPromptTemplate.from_messages(fact_template)
+prompt_translate_template = ChatPromptTemplate.from_messages(translate_template)
+
+# Prepare the prompt parameters
 fact_data = {
     'game': 'football',
     'tournament': 'World cup',
     'year': 2022
 }
-
-fact_prompt_template = ChatPromptTemplate.from_messages(fact_template)
-fact_prompt = fact_prompt_template.invoke(fact_data)
-fact_response = model.invoke(fact_prompt)
-
-translate_template = [
-    ("system", "You are a translation expert"),
-    ("human", "Translate the text {text} to {language}")
-]
 translation_data = {
-    'text': fact_response.content,
-    'language': 'Portugal'
+    'language': 'French'
 }
-translate_prompt_template = ChatPromptTemplate.from_messages(translate_template)
-translate_prompt = translate_prompt_template.invoke(translation_data)
-translation_response = model.invoke(translate_prompt)
 
-print(fact_response.content, translation_response.content, sep='\n\n')
+# create runnable lambda
+prompt_translate = RunnableLambda(lambda output: prompt_translate_template.format_prompt(text=output, **translation_data))
+
+# create the chain
+chain = prompt_fact_template | model | StrOutputParser() | prompt_translate | model | StrOutputParser()
+
+# invoke the chain
+response = chain.invoke(fact_data)
+
+print(response)
+
+# translation_data = {
+#     'text': fact_response.content,
+#     'language': 'Portugal'
+# }
+
+# fact_prompt_template = ChatPromptTemplate.from_messages(fact_template)
+# fact_prompt = fact_prompt_template.invoke(fact_data)
+# fact_response = model.invoke(fact_prompt)
+
+# translate_prompt_template = ChatPromptTemplate.from_messages(translate_template)
+# translate_prompt = translate_prompt_template.invoke(translation_data)
+# translation_response = model.invoke(translate_prompt)
+
+# print(fact_response.content, translation_response.content, sep='\n\n')
