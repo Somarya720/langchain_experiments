b920ab100665a315a0c2a26c7afc8a091a4c4dab
Somarya720 <somaryabhattacharyya@gmail.com>
2025-09-20 19:29:11 +0530

    split texts and create embeddings

--- RAG/create_vector_store.py
+++ RAG/create_vector_store.py
@@ -1,8 +1,12 @@
 from langchain_chroma import Chroma
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
 import os
 import sys
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # get required directories
 doc_name = 'lord_of_the_rings.txt'
@@ -17,4 +21,19 @@ if os.path.exists(persistant_directory):
 if not os.path.exists(doc_path):
     raise FileNotFoundError(f"Doc Name {doc_name} does not exist")
 
-db = Chroma.from_documents(documents='', embedding='', persist_directory=persistant_directory)
+# Load document 
+loader = TextLoader(doc_path)
+document = loader.load()
+
+# Split into chunks of 1000 size each with overlap of 50
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+document_list = text_splitter.split_documents(document)
+
+print(document_list[0].page_content)
+print(f"number of chunks: {len(document_list)}")
+
+embedding = OpenAIEmbeddings(
+    model='text-embedding-3-small'
+)
+
+db = Chroma.from_documents(documents=document_list, embedding=embedding, persist_directory=persistant_directory)
