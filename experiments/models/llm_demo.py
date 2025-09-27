1a6e1cf5bf17b317c6f767a8bfc6ced4de1a25f1
Somarya720 <somaryabhattacharyya@gmail.com>
2025-09-27 19:43:40 +0530

    add comment in llm_demo

--- experiments/models/llm_demo.py
+++ experiments/models/llm_demo.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = OpenAI(model='gpt-3.5-turbo-instruct')
+model = OpenAI(model='gpt-3.5-turbo-instruct') # gpt-3.5-turbo-instruct not a chatmodel, it is a llm
 
 result = model.invoke("What is the capital of India?")
 
