0318f4dcef2a47e999867045934509b2cec47ce6
Somarya720 <somaryabhattacharyya@gmail.com>
2025-09-28 02:22:46 +0530

    add hugging face pipeline

--- experiments/models/chat_model_hf_pipeline.py
+++ experiments/models/chat_model_hf_pipeline.py
@@ -1,26 +1,24 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 from dotenv import load_dotenv
 import os
 
 current_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 os.environ['HF_HOME'] = os.path.join(current_path, 'huggingface_cache')
-print(os.environ['HF_HOME'])
-# load_dotenv()
 
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 
+load_dotenv()
 
-# llm = HuggingFacePipeline(
-#     model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
-#     task = 'text-generation',
-#     pipeline_kwargs=dict(
-#         max_new_tokens = 100,
-#         temperature = 0.5
-#     )
-# )
+llm = HuggingFacePipeline.from_model_id(
+    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+    task = 'text-generation',
+    pipeline_kwargs=dict(
+        max_new_tokens = 100,
+        temperature = 0.2
+    )
+)
 
-# model = ChatHuggingFace(llm=llm)
+model = ChatHuggingFace(llm=llm)
 
-# result = model.invoke("What is the capital of India?")
+result = model.invoke("What is the capital of India?")
 
-# print(result.content)
+print(result)
