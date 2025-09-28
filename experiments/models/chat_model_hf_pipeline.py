9248d464a94206dfd84e54e5a45b25655977ec15
Somarya720 <somaryabhattacharyya@gmail.com>
2025-09-28 12:11:38 +0530

    remove loading of dot env

--- experiments/models/chat_model_hf_pipeline.py
+++ experiments/models/chat_model_hf_pipeline.py
@@ -1,12 +1,9 @@
-from dotenv import load_dotenv
 import os
 
 current_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.environ['HF_HOME'] = os.path.join(current_path, 'huggingface_cache')
 
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
-
-load_dotenv()
 
 llm = HuggingFacePipeline.from_model_id(
     model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
@@ -21,4 +18,4 @@ model = ChatHuggingFace(llm=llm)
 
 result = model.invoke("What is the capital of India?")
 
-print(result)
+print(result.content)
